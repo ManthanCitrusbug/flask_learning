faaeca57c0163043a116ba43d9bc0f3391a70325
ManthanCitrusbug <manthan.citrusbug@gmail.com>
2022-09-30 17:22:56 +0530

    Learned about flask models

--- flask.tutorial.py
+++ flask.tutorial.py
@@ -8,14 +8,15 @@ app.config['SECRET_KEY'] = 'e670ea4ac75d3720402ec47951f08494'
 def index(): 
     form = RegistraionForm()
     if form.validate_on_submit():
-        flash("Registered!", "success")
-        redirect(url_for("home"))
+        print(form.username.errors)
+        flash(f"Registered successfully {form.username.data}", "success")
+        return redirect(url_for("home"))
     return render_template('register.html', form=form)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return "flask tutorial..."
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
